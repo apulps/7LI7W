d1933fe720a1c0ff3b952fd018d63b67ca248cec
apulps <apinedaminon@gmail.com>
2020-11-01 23:04:02 +0100

    Add 'to do' file #1

---
+++ Week1_Ruby/Day_1_Finding_a_Nanny/ss_to_do.rb
@@ -0,0 +1,28 @@
+# Print the string "Hello, world."
+puts 'Hello, world'
+
+
+# For the string "Hello, Ruby." find the index of the word "Ruby."
+s = "Hello, Ruby."
+index = s.index('Ruby.')
+puts "The index of the word 'Ruby.' in #{s} is #{index}."
+
+
+# Print your name ten times
+i = 0
+while i < 10
+    puts 'Adrián'
+    i = i + 1
+end
+
+
+# Print the string "This is sentence number 1." where the number 1 changes from 1 to 10.
+i = 1
+while i < 11
+    puts "This is sentence number #{i}."
+    i = i + 1
+end
+
+
+# Run a Ruby program from a file.
+# From the command line: ~\Week1_Ruby\Day_1_Finding_a_Nanny\ss_to_do.rb
